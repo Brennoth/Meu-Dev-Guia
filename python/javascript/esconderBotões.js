374d2895185e14b0ee0bd3ffe7715b377817991d
Brenno Thiago <167652253+Brennoth@users.noreply.github.com>
2025-11-09 22:02:54 -0300

    meu dev guia

---
+++ python/javascript/esconderBotões.js
@@ -0,0 +1,18 @@
+
+/*Esconde o botão do link de navegação da IA*/
+function esconderBotaoIa() {
+    const liIA = document.querySelector(".li-IA")
+    liIA.classList.toggle("open")
+}
+
+
+/*Esconde o botão do link de navegação da Biblioteca*/
+function esconderBotaoBiblioteca() {
+    const liBibliotecas = document.querySelector(".li-bibliotecas")
+    liBibliotecas.classList.toggle("open")
+}
+
+function esconderTitleIa() {
+    const bibliotecasIa = document.querySelector("#bibliotecas-ia") 
+    bibliotecasIa.classList.toggle("openTitle")
+}
