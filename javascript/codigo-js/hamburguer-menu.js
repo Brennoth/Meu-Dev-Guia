18a38340bbc76df62eddf619a66266f11127eb70
Brenno Thiago <167652253+Brennoth@users.noreply.github.com>
2025-11-27 17:27:06 -0300

    Responsivo
    coloquei o responsivo no javascript, o hamburguer menu

--- javascript/codigo-js/hamburguer-menu.js
+++
@@ -1,8 +0,0 @@
-const menuBTN = document.querySelector(".menu-btn")
-const links = document.querySelector(".links")
-
-menuBTN.addEventListener("change", function () {
-    if (this.checked) {
-        links.style.maxHeight = "100vh";
-    }
-})
